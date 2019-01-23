0f0d476754c2a08f8c5895932165376d046155d5
Marcin Pajak <m.pajak@relay42.com>
2019-01-23 20:46:15 +0100

    Add the proper implementation of addN

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
-export const addN = a => b => 2;
+/**
+ * Adds @param toAdd to bounded @param n
+ * example: addN(8)(13) returns 21
+ */
+export const addN = (n: number): ((toAdd: number) => number) => (
+  toAdd: number
+): number => n + toAdd;
